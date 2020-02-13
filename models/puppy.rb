2f4713aa7e710dca2504d48f3b7c587f0d2377a2
Riley Slayden <slaydenriley@gmail.com>
2020-02-13 18:35:15 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,7 +3,7 @@ class Puppy
   attr_reader :breed
   attr_accessor :age, :name
 
-  def initialize(name, age, breed)
+  def initialize(name, breed, age)
     @name = name
     @age = age
     @breed = breed
